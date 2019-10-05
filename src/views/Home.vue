7de8ec8271d5fd5f5a215345dd83cc576a27818c
tricknp <np_patrick@yahoo.com>
2019-10-05 13:31:49 -0300

    🎉 initial structure added 🎉

---
+++ src/views/Home.vue
@@ -0,0 +1,20 @@
+<template lang="pug">
+    
+    div
+        Header
+
+</template>
+
+<script>
+import Header from '@/components/FixedComponents/Header'
+
+export default {
+    name: 'Home',
+    
+    components: {
+        Header
+    },
+
+
+}
+</script>
